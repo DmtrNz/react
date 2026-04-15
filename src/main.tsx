13289c3c5192b6a516ba0186dd95977b7acf05ba
DmtrNz <naz.dmtr@gmail.com>
2026-04-15 15:39:27 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 // Создаем контекст с начальным значением 0
 const CountContext = createContext<number>(0);
-
 // Редьюсер для управления состоянием счетчика
 const countReducer = (state: number, action: { type: "increment" }) => {
     console.log("1"); //
@@ -13,45 +12,35 @@ const countReducer = (state: number, action: { type: "increment" }) => {
 // Мемоизированный компонент, использующий контекст
 const ContextConsumer = memo(() => {
     console.log("2"); //2
-
     const count = useContext(CountContext);
-
     useEffect(() => {
         console.log("3");  //4
     }, []);
-
     return <div>{count}</div>;
 });
 
 // Мемоизированный компонент, использующий ref
 const RefComponent = memo(() => {
     console.log("4"); //3
-
     const divRef = useRef<HTMLDivElement>(null);
-
     useEffect(() => {
         if (divRef.current) {
             console.log("5");  //5
         }
     }, []);
-
     return <div ref={divRef} />;
 });
 
 // Родительский компонент, содержащий состояние и эффекты
 const ParentComponent = () => {
     console.log("6"); //1
-
     const [count, dispatch] = useReducer(countReducer, 0);
-
     useEffect(() => {
         console.log("7"); //6
-
         setTimeout(() => {
             dispatch({ type: "increment" });
         }, 3000);
     }, []);
-
     return (
         <CountContext.Provider value={count}>
             <ContextConsumer />
